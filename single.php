aea6e750070ddc88b2bbf9b092e11e72797f2553
Kartik Singh <62024947+KartikSinghK@users.noreply.github.com>
2020-08-06 18:00:46 +0530

    changed the theme

--- single.php
+++ single.php
@@ -4,26 +4,20 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
- * @package ECell-_Aryabhatta
+ * @package WP_Bootstrap_Starter
  */
 
-get_header();
-?>
+get_header(); ?>
 
-	<main id="primary" class="site-main">
+	<section id="primary" class="content-area col-sm-12 col-lg-8">
+		<main id="main" class="site-main" role="main">
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
+		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+			get_template_part( 'template-parts/content', get_post_format() );
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'ecell-aryabhatta' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'ecell-aryabhatta' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+			    the_post_navigation();
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
@@ -33,7 +27,8 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 
-	</main><!-- #main -->
+		</main><!-- #main -->
+	</section><!-- #primary -->
 
 <?php
 get_sidebar();
